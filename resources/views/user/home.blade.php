4f6f1a9fadddd7d49a1903489dd830e8ca321753
tayyab aslam <tayyabaslam771@gmail.com>
2021-07-20 02:28:38 +0500

    integration of frontend screens

---
+++ resources/views/user/home.blade.php
@@ -0,0 +1,310 @@
+@extends('user.master')
+@section('main')
+<!-- Hero Section Begin -->
+      <section class="hero">
+        <div class="hero__slider owl-carousel">
+           <div class="hero__items set-bg" data-setbg="{{asset('img/hero/rainer.jpg')}}">
+              <div class="hero__text">
+                 <h2>Quality is not only our standard.</h2>
+                 <a href="#" class="primary-btn">See Project</a>
+                 <div class="hero__social">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                 </div>
+              </div>
+           </div>
+           <div class="hero__items set-bg" data-setbg="{{asset('img/hero/leung.jpg')}}">
+              <div class="hero__text">
+                 <h2>Quality is not only our standard.</h2>
+                 <a href="#" class="primary-btn">See Project</a>
+                 <div class="hero__social">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                 </div>
+              </div>
+           </div>
+           <div class="hero__items set-bg" data-setbg="{{asset('img/hero/emerson.jpg')}}">
+              <div class="hero__text">
+                 <h2>Quality is not only our standard.</h2>
+                 <a href="#" class="primary-btn">See Project</a>
+                 <div class="hero__social">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                 </div>
+              </div>
+           </div>
+           <div class="hero__items set-bg" data-setbg="{{asset('img/hero/lasse.jpg')}}">
+              <div class="hero__text">
+                 <h2>Quality is not only our standard.</h2>
+                 <a href="#" class="primary-btn">See Project</a>
+                 <div class="hero__social">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                 </div>
+              </div>
+           </div>
+        </div>
+        <div class="slide-num" id="snh-1"></div>
+        <div class="slider__progress"><span></span></div>
+     </section>
+     <!-- Hero Section End -->
+     <!-- About Section Begin -->
+     <section class="about spad">
+        <div class="container">
+           <div class="row">
+              <div class="col-lg-6">
+                 <div class="about__text">
+                    <div class="section-title">
+                       <span>who are we</span>
+                       <h2>We propose and discuss design rules</h2>
+                    </div>
+                    <div class="about__para__text">
+                       <p>Metasurfaces are generally designed by placing scatterers in periodic or pseudo-periodic
+                          grids. We propose and discuss design rules for functional metasurfaces with randomly
+                          placed.
+                       </p>
+                       <p>Anisotropic elements that randomly sample. Quisque sit amet nisl ante. Fusce lacinia non
+                          tellus id gravida. Cras neque dolor, volutpat et hendrerit et.
+                       </p>
+                    </div>
+                    <a href="about.html" class="primary-btn normal-btn">Learn More</a>
+                 </div>
+              </div>
+              <div class="col-lg-6">
+                 <div class="about__pic">
+                    <div class="about__pic__inner">
+                       <img src="img/about-pic.jpg" alt="">
+                    </div>
+                 </div>
+              </div>
+           </div>
+        </div>
+     </section>
+     <!-- About Section End -->
+     <!-- Project Section Begin -->
+     <section class="project spad">
+        <div class="container-fluid">
+           <div class="row">
+              <div class="col-lg-12 text-center">
+                 <div class="section-title">
+                    <span>Get Franchise</span>
+                    <h2>Our Franchises</h2>
+                 </div>
+              </div>
+           </div>
+           <div class="row">
+              <div class="project__slider owl-carousel">
+                 <div class="col-lg-3">
+                    <div class="project__slider__item set-bg" data-setbg="img/project/project-42.jpg">
+                       <div class="project__slider__item__hover">
+                          <a href="#">
+                             <span>Click to Visit</span>
+                             <h5>Canal Road Branch</h5>
+                          </a>
+                       </div>
+                    </div>
+                 </div>
+                 <div class="col-lg-3">
+                    <div class="project__slider__item set-bg" data-setbg="img/project/newpro.jpg">
+                       <div class="project__slider__item__hover">
+                          <a href="#">
+                             <span>Click to Visit</span>
+                             <h5>Emporium Food Court</h5>
+                          </a>
+                       </div>
+                    </div>
+                 </div>
+                 <div class="col-lg-3">
+                    <div class="project__slider__item set-bg" data-setbg="img/project/IMG-20180211-WA0031.jpg">
+                       <div class="project__slider__item__hover">
+                          <a href="#">
+                             <span>Click to Visit</span>
+                             <h5>Lower Mall</h5>
+                          </a>
+                       </div>
+                    </div>
+                 </div>
+                 <div class="col-lg-3">
+                    <div class="project__slider__item set-bg" data-setbg="img/project/newpro.jpg">
+                       <div class="project__slider__item__hover">
+                          <a href="#">
+                             <span>Click to Visit</span>
+                             <h5>Karachi Seview</h5>
+                          </a>
+                       </div>
+                    </div>
+                 </div>
+                 <div class="col-lg-3">
+                    <div class="project__slider__item set-bg" data-setbg="img/project/IMG-20180211-WA0031.jpg">
+                       <div class="project__slider__item__hover">
+                          <a href="#">
+                             <span>interiors</span>
+                             <h5>Lower River Street Astoria</h5>
+                          </a>
+                       </div>
+                    </div>
+                 </div>
+              </div>
+           </div>
+        </div>
+     </section>
+     <!-- Project Section End -->
+     <!-- Gallery Start -->
+     <section class="gallery spad">
+        <div class="container text-center ">
+           <div class="row">
+              <div class="col-lg-12 text-center">
+                 <div class="section-title">
+                    <span>Our</span>
+                    <h2>Portfolio</h2>
+                 </div>
+              </div>
+           </div>
+           <div class="portfolio-menu">
+              <ul>
+                 <li class="active" data-filter="*">All</li>
+                 <li data-filter=".web">Food</li>
+                 <li data-filter=".seo">Franchises</li>
+                 <li data-filter=".graphics">Warehouse</li>
+              </ul>
+           </div>
+           <div class="portfolio-item">
+              <div class="item web">
+                 <div style="background-image: url(http://www.esse90.it/wp-content/uploads/2017/07/img-placeholder.png);"></div>
+              </div>
+              <div class="item web">
+                 <div style="background-image: url(https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=350&h=150);"></div>
+              </div>
+              <div class="item seo">
+                 <div style="background-image: url(http://unitel.com.br/wp-content/uploads/2014/03/img-3.gif);"></div>
+              </div>
+              <div class="item graphics">
+                 <div style="background-image: url(http://www.dracorion.com/wp-content/uploads/2012/03/placeholder4-480x300-crop.jpg);"></div>
+              </div>
+              <div class="item seo">
+                 <div style="background-image: url(http://dummyimage.com/600x250/666666/fff&text=Dynamic+Dummy+Image+Generator);"></div>
+              </div>
+              <div class="item web">
+                 <div style="background-image: url(http://fpoimg.com/600x250/For%20Place%20Only%20Image);"></div>
+              </div>
+           </div>
+        </div>
+     </section>
+     <!-- Gallery End -->
+     <!-- Our Clients start -->
+     <section class="spad">
+        <div class="container">
+           <div class="row">
+              <div class="col-lg-12 text-center">
+                 <div class="section-title">
+                    <span>Our</span>
+                    <h2>Vendors</h2>
+                 </div>
+              </div>
+           </div>
+           <div class="row">
+              <div class="col-lg-12">
+                 <div class="logo__carousel owl-carousel">
+                    <div class="logo__carousel__item">
+                       <a href="#"><img src="{{asset('img/logo/logo-1.png')}}" alt=""></a>
+                    </div>
+                    <div class="logo__carousel__item">
+                       <a href="#"><img src="{{asset('img/logo/logo-2.png')}}" alt=""></a>
+                    </div>
+                    <div class="logo__carousel__item">
+                       <a href="#"><img src="{{asset('img/logo/logo-3.png')}}" alt=""></a>
+                    </div>
+                    <div class="logo__carousel__item">
+                       <a href="#"><img src="{{asset('img/logo/logo-4.png')}}" alt=""></a>
+                    </div>
+                    <div class="logo__carousel__item">
+                       <a href="#"><img src="{{asset('img/logo/logo-5.png')}}" alt=""></a>
+                    </div>
+                    <div class="logo__carousel__item">
+                       <a href="#"><img src="{{asset('img/logo/logo-3.png')}}" alt=""></a>
+                    </div>
+                 </div>
+              </div>
+           </div>
+        </div>
+     </section>
+     <!-- Our Clients End -->
+     <!-- Latest Section Begin -->
+   <section class="latest spad">
+       <div class="container-fluid">
+           <div class="row">
+               <div class="col-md-12">
+                   <div class="section-title text-center">
+                       <span>Latest News</span>
+                       <h2>From our blog</h2>
+                   </div>
+               </div>
+               </div>
+           </div>
+           <div class="row">
+               <div class="col-lg-4 col-md-6 col-sm-6">
+                   <div class="latest__item">
+                       <div class="latest__item__pic">
+                           <img src="{{asset('img/blog/latest-1.jpg')}}" alt="">
+                       </div>
+                       <div class="latest__item__text">
+                           <span>Architecture</span>
+                           <h4>Target and Amazon Shopping List for Home Stagers</h4>
+                           <a href="#">Read more</a>
+                       </div>
+                   </div>
+               </div>
+               <div class="col-lg-4 col-md-6 col-sm-6">
+                   <div class="latest__item">
+                       <div class="latest__item__pic">
+                           <img src="{{asset('img/blog/latest-2.jpg')}}" alt="">
+                       </div>
+                       <div class="latest__item__text">
+                           <span>Interior</span>
+                           <h4>6 Ideas for Team Building and Employee Appreciation for Home Stagers</h4>
+                           <a href="#">Read more</a>
+                       </div>
+                   </div>
+               </div>
+               <div class="col-lg-4 col-md-6 col-sm-6">
+                   <div class="latest__item">
+                       <div class="latest__item__pic">
+                           <img src="{{asset('img/blog/latest-3.jpg')}}" alt="">
+                       </div>
+                       <div class="latest__item__text">
+                           <span>Planning</span>
+                           <h4>How to Find the Best Price Structure for Your Home Staging Services</h4>
+                           <a href="#">Read more</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </section>
+   <!-- Latest Section End -->
+     <!-- Call To Action Section Begin -->
+     <section class="callto spad set-bg" data-setbg="{{asset('img/call-bg.jpg')}}">
+        <div class="container">
+           <div class="row d-flex justify-content-center">
+              <div class="col-lg-10 text-center">
+                 <div class="callto__text">
+                    <span>Why choose us?</span>
+                    <h2>Our Ability To Deliver Outstanding Results For Our Clients Starts With Our Team Of Smart.
+                    </h2>
+                    <a href="contact.html" class="primary-btn">Contact Us</a>
+                 </div>
+              </div>
+           </div>
+        </div>
+     </section>
+     <!-- Call To Action Section End -->
+
+     @endsection
---
+++ resources/views/user/master.blade.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="zxx">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="description" content="FC-Frichicks">
+      <meta name="keywords" content="FC,Frichicks,fastfood,Pizza,Burger">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>FC-FriChicks</title>
+      <!-- Google Font -->
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
+         rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Aldrich&display=swap" rel="stylesheet">
+      <!-- Css Styles -->
+      <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" type="text/css">
+      <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}" type="text/css">
+      <link rel="stylesheet" href="{{asset('css/elegant-icons.css')}}" type="text/css">
+      <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}" type="text/css">
+      <link rel="stylesheet" href="{{asset('css/slicknav.min.css')}}" type="text/css">
+      <link rel="stylesheet" href="{{asset('css/slick.css')}}" type="text/css">
+      <link rel="stylesheet" href="{{asset('css/style.css')}}" type="text/css">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+   </head>
+   <body>
+      <!-- Page Preloder -->
+      <div id="preloder">
+         <div class="loader"></div>
+      </div>
+      <!-- Offcanvas Menu Begin -->
+      <div class="offcanvas-menu-overlay"></div>
+      <div class="offcanvas-menu-wrapper">
+         <div class="offcanvas__logo">
+            <a href="#"><img src="img/logo.png" alt=""></a>
+         </div>
+         <div id="mobile-menu-wrap"></div>
+         <!-- <div class="offcanvas__widget">
+            <span>Call us for any questions</span>
+            <h4>+01 123 456 789</h4>
+            </div> -->
+      </div>
+      <!-- Offcanvas Menu End -->
+      <!-- Header Section Begin -->
+      <header class="header">
+         <div class="container-fluid">
+            <div class="row">
+               <div class="col-lg-3">
+                  <div class="header__logo">
+                     <a href="./index.html"><img src="img/logo.png" alt=""></a>
+                  </div>
+               </div>
+               <div class="col-lg-9">
+                  <nav class="header__menu mobile-menu">
+                     <ul>
+                        <li class="active"><a href="./index.html">Home</a></li>
+                        <li><a href="./about.html">About us</a></li>
+                        <li><a href="./Our Franchise.html">Get Frenchise</a></li>
+                        <li><a href="./gallery.html">Our Gallery</a></li>
+                        <li><a href="./our vendors.html">Our Clients</a></li>
+                        <li><a href="./contact.html">Contact us</a></li>
+                        <li>
+                           <a href="#">More</a>
+                           <ul class="dropdown">
+                              <li><a href="./profile.html">Profile</a></li>
+                              <li><a href="./faq.html">FAQ's</a></li>
+                              <li><a href="./news.html">News</a></li>
+                              <li><a href="./warehouse.html">Warehouse</a></li>
+                              <li><a href="./careers.html">Careers</a></li>
+                              <li><a href="./downloads.html">Downloads</a></li>
+                           </ul>
+                        </li>
+                        <li class="order-o"><a class="button-o" data-toggle="modal" data-target="#exampleModal" href="./contact.html">Order Now</a></li>
+                     </ul>
+                  </nav>
+               </div>
+            </div>
+            <div class="canvas__open"><i class="fa fa-bars"></i></div>
+         </div>
+      </header>
+      <!-- Header Section End -->
+      <!-- Model -->
+      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+         <div class="modal-dialog" role="document">
+            <div class="modal-content">
+               <div class="modal-header">
+                  <h5 class="modal-title text-center" id="exampleModalLabel">Fill The Form & Get 25% Discount</h5>
+               </div>
+               <div class="modal-body faq">
+                  <div class="container">
+                     <form action="/action_page.php">
+                        <div class="row">
+                           <div class="col-md-3">
+                              <label for="name">Name</label>
+                           </div>
+                           <div class="col-md-9">
+                              <input type="text" id="name" name="firstname" placeholder="Your name.." required>
+                           </div>
+                        </div>
+                        <div class="row">
+                           <div class="col-md-3">
+                              <label for="email">Email</label>
+                           </div>
+                           <div class="col-md-9">
+                              <input type="email" id="email" name="email" placeholder="name@gmail.com"required>
+                           </div>
+                        </div>
+                        <div class="row">
+                           <div class="col-md-3">
+                              <label for="phone">Phone No</label>
+                           </div>
+                           <div class="col-md-9">
+                              <input type="tel" id="phone" name="phone" placeholder="0321 0123456"required>
+                           </div>
+                        </div>
+                        <div class="row">
+                           <div class="col-md-3">
+                              <label for="city">City</label>
+                           </div>
+                           <div class="col-md-9">
+                              <select id="city" name="city" required>
+                                 <option value="australia">Australia</option>
+                                 <option value="canada">Canada</option>
+                                 <option value="usa">USA</option>
+                              </select>
+                           </div>
+                        </div>
+                     </form>
+                  </div>
+               </div>
+               <div class="modal-footer">
+                  <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-warning">Submit to get discount</button>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      @yield('main')
+      <!-- Footer Section Begin -->
+      <footer class="footer set-bg" data-setbg="img/footer-bg.jpg">
+         <div class="container">
+            <div class="footer__top">
+               <div class="row">
+                  <div class="col-lg-8 col-md-6">
+                     <div class="footer__top__text">
+                        <h2>Ready To Work With Us?</h2>
+                     </div>
+                  </div>
+                  <div class="col-lg-4 col-md-6">
+                     <div class="footer__top__newslatter">
+                        <form action="#">
+                           <input type="text" placeholder="Enter your email...">
+                           <button type="submit"><i class="fa fa-send"></i></button>
+                        </form>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="row text-white" >
+               <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="footer__about">
+                     <div class="footer__logo">
+                        <a href="#"><img src="img/logo.png" alt=""></a>
+                     </div>
+                     <p>7176 Blue Spring Lane <br />Santa Monica, CA 90403</p>
+                     <ul>
+                        <li>Info@gmail.com</li>
+                        <li>+92 123 456 789</li>
+                     </ul>
+                     <div class="footer__social">
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-instagram"></i></a>
+                        <a href="#"><i class="fa fa-linkedin"></i></a>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-lg-2 offset-lg-1 col-md-3 col-sm-6">
+                  <div class="footer__widget">
+                     <h6>Company</h6>
+                     <ul>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Get Franchise</a></li>
+                        <li><a href="#">Our Gallery</a></li>
+                        <li><a href="#">Clients</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                     </ul>
+                  </div>
+               </div>
+               <div class="col-lg-2 col-md-3 col-sm-6">
+                  <div class="footer__widget">
+                     <h6>Services</h6>
+                     <ul>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">Downloads</a></li>
+                        <li><a href="#">Profile</a></li>
+                        <li><a href="#">Profile</a></li>
+                     </ul>
+                  </div>
+               </div>
+               <div class="col-lg-4 col-md-6 col-sm-6">
+                  <div class="footer__address">
+                     <h6>Get In Touch</h6>
+                     <p>7176 Blue Spring Lane <br />Santa Monica, CA 90403</p>
+                     <ul>
+                        <li>Info@gmail.com</li>
+                        <li>+92 123 456 789</li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+            <div class="copyright">
+               <div class="row text-white">
+                  <div class="col-lg-8 col-md-7">
+                     <div class="copyright__text">
+                        <p>
+                           Copyright © <script>
+                              document.write(new Date().getFullYear());
+                           </script> All rights reserved | This website is design <i class="fa fa-heart-o"
+                              aria-hidden="true"></i> by <a href="https://gigacreatives.com"
+                              target="_blank">Giga Creatives</a>
+                        </p>
+                     </div>
+                  </div>
+                  <div class="col-lg-4 col-md-5">
+                     <div class="copyright__widget">
+                        <a href="#">Terms of use</a>
+                        <a href="#">Privacy Policy</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- Footer Section End -->
+      <!-- Js Plugins -->
+      <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
+      <script src="{{asset('js/bootstrap.min.js')}}"></script>
+      <script src="{{asset('js/jquery.slicknav.js')}}"></script>
+      <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+      <script src="{{asset('js/slick.min.js')}}"></script>
+      <script src="{{asset('js/main.js')}}"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.min.js"></script>
+   </body>
+</html>
